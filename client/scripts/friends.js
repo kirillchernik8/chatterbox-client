4574215b7068741d330df921d972b83d2d1322af
Joseph Pranay Martin <jpranaymartin@gmail.com>
2019-03-01 07:42:15 -0500

    (init)

---
+++ client/scripts/friends.js
@@ -0,0 +1,12 @@
+// This object houses all the friend _data_ for the app.
+// Treat it like a data structure - add methods to interact
+// with and manipulate the data.
+var Friends = {
+
+  // TODO: Define how you want to store your list of friends.
+  storage: null
+
+  // TODO: Define methods which allow you to add, toggle, 
+  // and check the friendship status of other users.
+
+};
